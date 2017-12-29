26833b3a50e0f01b9926cdfabe744a3b844bf356
nikolay <nikolayynayydenov@gmail.com>
2017-12-29 15:31:21 +0200

    create the fisrt components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router'
+import { routes } from './app.routes'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component'
+import { LoginComponent } from './components/auth/login/login.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { RegisterComponent } from './components/auth/register/register.component'
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        HeaderComponent,
+        FooterComponent,
+        RegisterComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
