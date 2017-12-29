0edb75c877518071bf1cfce969d7cda044558407
Silvena Shtereva <silvenag@gmail.com>
2017-12-29 19:05:54 +0200

    header, footer, wellcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { WellcomePageComponent } from './components/wellcome-page/wellcome-page.component';
+import { FooterPageComponent } from './components/footer-page/footer-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WellcomePageComponent,
+    FooterPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/wellcome-page/wellcome-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, NgModule } from '@angular/core';
+// import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-wellcome-page',
+  templateUrl: './wellcome-page.component.html',
+  styleUrls: ['./wellcome-page.component.css'],
+ // providers: [NgbCarouselConfig]
+})
+export class WellcomePageComponent  {
+
+  constructor() { /*
+    config: NgbCarouselConfig
+    config.interval = 10000;
+    config.wrap = true;
+    config.keyboard = false;*/
+  }
+
+}
