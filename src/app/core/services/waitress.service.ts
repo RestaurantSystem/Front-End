f333143d7c96e5e44b5b63aab7c6f18998e0a71a
Silvena Shtereva <silvenag@gmail.com>
2018-01-12 12:33:45 +0200

    port

--- src/app/core/services/waitress.service.ts
+++ src/app/core/services/waitress.service.ts
@@ -3,21 +3,21 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from './../../../environments/environment';
 
 const port = environment.serverPort;
-const url = `http://localhost:${port}/Manager`;
+const url = `http://localhost:${port}/Waitress`;
 
 @Injectable()
 export class WaitressService {
     constructor(private http: HttpClient) {}
 
     getAllTables() {
-        return this.http.get(url + '/waitress/all-tables');
+        return this.http.get(url + '/All-tables');
     }
 
     getTables() {
-        return this.http.get(url + '/waitress/tables');
+        return this.http.get(url + '/Tables');
     }
 
     getBills() {
-        return this.http.get(url + '/waitress/bills');
+        return this.http.get(url + '/Bills');
     }
 }
