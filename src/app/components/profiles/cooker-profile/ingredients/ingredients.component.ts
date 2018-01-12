72e7f6b65f761245e29d5ae45a5d380e8a067baa
nikolay <nikolayynayydenov@gmail.com>
2018-01-12 12:49:10 +0200

    show products and ingredients in cook profile

--- src/app/components/profiles/cooker-profile/ingredients/ingredients.component.ts
+++ src/app/components/profiles/cooker-profile/ingredients/ingredients.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { CookService } from './../../../../core/services/cook.service'
 
 @Component({
-  selector: 'app-ingredients',
-  templateUrl: './ingredients.component.html',
-  styleUrls: ['./ingredients.component.css']
+    selector: 'app-ingredients',
+    templateUrl: './ingredients.component.html',
+    styleUrls: ['./ingredients.component.css']
 })
 export class IngredientsComponent implements OnInit {
+    public ingredients: object[]
 
-  constructor() { }
+    constructor(private cookService: CookService) {
+        this.ingredients = []
+    }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.cookService.getAllIngredients()
+            .subscribe({
+                next: data => {
+                    this.ingredients = data['ingredients']
+                },
+                complete: () => {
+                    console.log('Ingredients loaded!')
+                    console.log(this.ingredients)
+                }
+            })
+    }
 }
--- src/app/components/profiles/cooker-profile/products/products.component.ts
+++ src/app/components/profiles/cooker-profile/products/products.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { CookService } from './../../../../core/services/cook.service'
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+    public products: object[]
 
-  constructor() { }
+    constructor(private cookService: CookService) {
+        this.products = []
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.cookService.getAllProducts()
+            .subscribe({
+                next: data => {
+                    this.products = data['products']
+                },
+                complete: () => {
+                    console.log('Products loaded!')
+                    console.log(this.products)
+                }
+            })
+    }
 
 }
